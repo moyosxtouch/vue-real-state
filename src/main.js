b4ad58682b7cd28bb4bb516a97f00d435c259aa0
moyo <moyosxtouch@gmail.com>
2023-11-23 19:55:59 -0600

    se agrego firebase al proyecto

--- src/main.js
+++ src/main.js
@@ -7,6 +7,11 @@ import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+
+//Firebase
+import { VueFire, VueFireAuth } from "vuefire";
+import { firebaseApp } from "./config/firebase";
+
 import App from "./App.vue";
 import router from "./router";
 
@@ -15,6 +20,11 @@ const app = createApp(App);
 const vuetify = createVuetify({
   components,
   directives,
+});
+//Firebase
+app.use(VueFire, {
+  firebaseApp,
+  modules: [VueFireAuth()],
 });
 app.use(createPinia());
 app.use(router);
