4c1612d5fd21b26ffde21dc8d688a6fdbdd23a37
moyo <moyosxtouch@gmail.com>
2023-11-28 14:26:06 -0600

    se modifico para poder cerrar sesion en firebase

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { RouterView } from "vue-router";
+import { useAuthStore } from "./stores/auth";
+const auth = useAuthStore();
 </script>
 
 <template>
@@ -11,8 +13,14 @@ import { RouterView } from "vue-router";
         </template>
 
         <template v-slot:append>
-          <v-btn :to="{ name: 'home' }"> Inicio</v-btn>
-          <v-btn :to="{ name: 'login' }"> Iniciar Sesión</v-btn>
+          <div v-if="auth.isAuth">
+            <v-btn :to="{ name: 'admin-propiedades' }"> Admin</v-btn>
+            <v-btn @click="auth.logout"> Cerrar Sesión</v-btn>
+          </div>
+          <div v-else>
+            <v-btn :to="{ name: 'home' }"> Inicio</v-btn>
+            <v-btn :to="{ name: 'login' }"> Iniciar Sesión</v-btn>
+          </div>
         </template>
       </v-app-bar>
 
--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,21 +1,35 @@
-import { ref, computed } from "vue";
+import { ref, computed, onMounted } from "vue";
 import { defineStore } from "pinia";
 import { useFirebaseAuth } from "vuefire";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import {
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
+import { useRouter } from "vue-router";
 
 export const useAuthStore = defineStore("auth", () => {
   const auth = useFirebaseAuth();
-  const authUser = ref({});
+  const authUser = ref(null);
+  const router = useRouter();
   const errorMsg = ref("");
   const errorCodes = {
     "auth/invalid-login-credentials": "Credenciales Incorrectas",
   };
+
+  onMounted(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        authUser.value = user;
+      }
+    });
+  });
   const login = ({ email, password }) => {
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         const user = userCredential.user;
         authUser.value = user;
-        console.log(authUser.value);
+        router.push({ name: "admin-propiedades" });
       })
       .catch((error) => {
         console.log(error);
@@ -25,9 +39,24 @@ export const useAuthStore = defineStore("auth", () => {
   const hasError = computed(() => {
     return errorMsg.value;
   });
+  const isAuth = computed(() => {
+    return authUser.value;
+  });
+  const logout = () => {
+    signOut(auth)
+      .then(() => {
+        authUser.value = null;
+        router.push({ name: "login" });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return {
     login,
+    logout,
     hasError,
     errorMsg,
+    isAuth,
   };
 });
