fe27e2e4737266c3bea2a233ffd93d0898f9a3a7
moyo <moyosxtouch@gmail.com>
2023-11-28 19:08:33 -0600

    se modifico para permitir el acceso si el usuario esta autentificado

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import { onAuthStateChanged } from "firebase/auth";
+import { useFirebaseAuth } from "vuefire";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -42,13 +44,33 @@ const router = createRouter({
 });
 
 //Guard de navegacion
-router.beforeEach((to, from, next) => {
+router.beforeEach(async (to, from, next) => {
   const requiresAuth = to.matched.some((url) => url.meta.requiresAuth);
   if (requiresAuth) {
     //Comprobar que el usuario este autentificado
+    try {
+      await authenticateUser();
+      next();
+    } catch (error) {
+      console.log(error);
+      next({ name: "login" });
+    }
   } else {
     //No esta protegido, mostramos la vista
     next();
   }
 });
+function authenticateUser() {
+  const auth = useFirebaseAuth();
+  return new Promise((resolve, reject) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      unsubscribe();
+      if (user) {
+        resolve();
+      } else {
+        reject();
+      }
+    });
+  });
+}
 export default router;
