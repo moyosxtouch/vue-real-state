bc273c16d5924131cadb5a5f9042b673932d50fe
moyo <moyosxtouch@gmail.com>
2023-12-01 19:46:11 -0600

    modificacion para consultar la db y mostrar las propiedades como cards

---
+++ src/components/Propiedad.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <v-col cols="12" md="4">
+    <h2>Propiedad...</h2>
+  </v-col>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,20 @@
-<script setup></script>
+<script setup>
+import usePropiedades from "../composables/usePropiedades";
+import Propiedad from "../components/Propiedad.vue";
+const { propiedadesCollection } = usePropiedades();
+</script>
 
 <template>
-  <h1 class="text-center text-h3 font-weight-bold my-5">Vue Bienes Raices</h1>
+  <h1 class="text-center text-h3 font-weight-bold my-5">
+    Nuestras Propiedades
+  </h1>
+  <v-card flat class="py-10">
+    <v-row>
+      <Propiedad
+        v-for="propiedad in propiedadesCollection"
+        :key="propiedad.id"
+        :propiedad="propiedad"
+      />
+    </v-row>
+  </v-card>
 </template>
